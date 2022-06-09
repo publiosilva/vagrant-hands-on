eac373d1fc591eac263ea7f90fd38db16eed0da1
Publio Blenilio <publio.blenilio@gmail.com>
2022-06-09 20:41:55 -0300

    add 2-puppet

---
+++ 2-puppet/bionic/src/index.php
@@ -0,0 +1,15 @@
+<?php
+echo "Testing connection... <br /> <br />";
+
+$servername = "192.168.1.150";
+$username = "phpuser";
+$password = "pass";
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected.";
+?>
